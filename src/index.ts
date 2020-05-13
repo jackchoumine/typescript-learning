590fb1d2e8346ac9177d62e7d47d995d35cabac2
jackzhoumine <jackzhoumine@163.com>
2020-05-14 01:38:13 +0800

    类1

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
+/*
+ * @Description: 应用入口
+ * @Date: 2019-12-02 02:12:29
+ * @Author: JackChouMine
+ * @LastEditTime: 2020-05-14 00:45:23
+ * @LastEditors: JackChouMine
+ */
 // import './example/basic-type'
 // import './example/symbol'
 // import './example/ES6-class-1'
 // import './example/interface'
-import './example/enumeration'
-import './example/func'
+// import './example/enumeration'
+// import './example/func'
+import './example/class'
