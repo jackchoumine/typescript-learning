b1825ab345113211e6c8ee4a30929d0fc9c8d466
jackzhoumine <jackzhoumine@163.com>
2020-05-23 19:55:53 +0800

    15-类型检查机制-兼容2

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
  * @Description: 应用入口
  * @Date: 2019-12-02 02:12:29
  * @Author: JackChouMine
- * @LastEditTime: 2020-05-21 01:51:08
+ * @LastEditTime: 2020-05-23 18:31:58
  * @LastEditors: JackChouMine
  */
 // import './example/basic-type'
@@ -14,4 +14,5 @@
 // import './example/class'
 // import './example/abstractClass';
 // import './example/generic-1';
-import './example/generic-2';
+// import './example/generic-2';
+import './example/type-check-2';
