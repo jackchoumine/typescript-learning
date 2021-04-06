693de49f4289801d14a5e7c767d75dcae4f97c6e
jack <jackzhoumine@gmail.com>
2021-04-06 17:40:52 +0800

    feat: 易混淆的类型

--- src/difficulty/type.ts
+++ src/difficulty/type.ts
@@ -4,5 +4,5 @@ const bar: any = 10
 // @ts-ignore
 const foo: unknown = 'string' //
 // foo.substr(1) // Error: 语法检查不通过报错
-;(foo as string).substr(1) // OK
+; (foo as string).substr(1) // OK
 // if (typeof foo === 'string') { foo.substr(1) } // OK
